e4d2dc0d628d3c3c9d94c54a7081384a321fbac8
yuan <zylucying@163.com>
2018-03-21 14:35:51 +0800

    2018-03-21 14:35pm 张渊

--- js/lib/app_config.js
+++ js/lib/app_config.js
@@ -2,10 +2,10 @@ var url = 'http://116.62.68.26:8080';//测试地址
 var Interface_url = 'http://116.62.68.26:8080';//接口的测试地址
 var img_url = 'http://116.62.68.26/';//图片的地址
 //var img_url = 'http://47.92.145.21:81/';//图片的地址
-function denglu_zt(){
+function denglu_zt(cookie){
 	mui.ajax(url + '/yskjApp/appYskj/V1/landState.do',{
 		data:{
-			"cookie":JSON.parse(localStorage.getItem('cookxs_yh')),
+			"cookie":cookie,
 		},
 		dataType:'json',
 		type:'post',
